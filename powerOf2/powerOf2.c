5d594d246be06660837cb1dc9c1b53b53f1e9067
Thang Phan <lameojicoemail@gmail.com>
2024-10-25 11:48:20 +0700

    third-commit

---
+++ powerOf2/powerOf2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int checkPowerOf2(int n) {
+    int i = 0;
+    int tmp = 0;
+    while (tmp < n)
+    {
+        tmp = pow(2, i);
+        if (tmp == n)
+        {
+            return 1;
+        }
+        else
+        {
+            i++;
+        }
+    }
+    return 0;
+}
+
+int main() {
+    printf("%d", checkPowerOf2(2));
+    return 0;
+}
